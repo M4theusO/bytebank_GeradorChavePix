9c1ad871be24c24cc2a557e937a46a69a2e007e6
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2023-08-28 15:55:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bytebank_GeradorChavePix
+ DLL para gerar chaves PIX para teste no projeto bytebank
---
+++ bytebank_GeradorChavePix/GeradorPix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_GeradorChavePix
+{
+    public static class GeradorPix
+    {
+        public static string GetChavePix()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public static List<string> GetChavesPix(int numeroDeChaves)
+        {
+            if(numeroDeChaves <= 0)
+            {
+                return null;
+            }
+            else
+            {
+                var chaves = new List<string>();
+                for (int i = 0; i < numeroDeChaves; i++)
+                {
+                    chaves.Add(Guid.NewGuid().ToString());
+                }
+                return chaves;
+            }
+        }
+    }
+}
